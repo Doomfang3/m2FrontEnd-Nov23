20b8332c8bdffdfad578d80a74fed505ad343a04
Doomfang3 <saubinmathieu@gmail.com>
2023-10-10 11:34:15 +0200

    Fix it

--- src/pages/GamersPage.jsx
+++ src/pages/GamersPage.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react'
 
 const GamersPage = () => {
   const [gamers, setGamers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const fetchGamers = async () => {
     const response = await fetch(`${import.meta.env.VITE_API_URL}/users?_embed=playSessions`)
     if (response.ok) {
       const parsed = await response.json()
       setGamers(parsed)
+      setIsLoading(false)
     }
   }
 
@@ -15,32 +17,33 @@ const GamersPage = () => {
     fetchGamers()
   }, [])
 
-  return (
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
     <>
       <h1>All the gamers</h1>
       <ul>
-        {gamers.length &&
-          gamers.map(gamer => (
-            <li key={gamer.id}>
-              <h3>{gamer.firstname}</h3>
-              <ul>
-                {gamer.hobbies.map(hobby => (
-                  <li key={hobby}>{hobby}</li>
-                ))}
-              </ul>
-              <h4>Play Sessions</h4>
-              <ul>
-                {gamer.playSessions.map(playSession => (
-                  <li key={playSession.id}>
-                    <p>
-                      {playSession.game} - {playSession.playTime} minutes
-                    </p>
-                    <p>{playSession.description}</p>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
+        {gamers.map(gamer => (
+          <li key={gamer.id}>
+            <h3>{gamer.firstname}</h3>
+            <ul>
+              {gamer.hobbies.map(hobby => (
+                <li key={hobby}>{hobby}</li>
+              ))}
+            </ul>
+            <h4>Play Sessions</h4>
+            <ul>
+              {gamer.playSessions.map(playSession => (
+                <li key={playSession.id}>
+                  <p>
+                    {playSession.game} - {playSession.playTime} minutes
+                  </p>
+                  <p>{playSession.description}</p>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
       </ul>
     </>
   )
